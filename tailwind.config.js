dc27a7f9b0f99ed52475f32938f6b30378dacc2a
LeonidasMesquita <112254118+LeonidasMesquita@users.noreply.github.com>
2024-04-21 11:43:11 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        "cor1": "#592C28",
+        "cor2": "#E4B18E",
+        "cor3": "#261420",
+        "cor4": "#8C5042",
+      },
+    },
+  },
+  plugins: [],
+}
